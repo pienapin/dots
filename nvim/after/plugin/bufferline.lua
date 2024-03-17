3e29bedc2a1604487022db717652ba8bb1dac026
Alvien Cenna <alpiencenna@gmail.com>
2024-03-17 09:56:46 +0700

    add: refind theme and config

--- nvim/after/plugin/bufferline.lua
+++ nvim/after/plugin/bufferline.lua
@@ -1,3 +1,18 @@
+require('bufferline').setup({
+	options = {
+		mode = 'buffers',
+		diagnostics = 'nvim_lsp',
+		offsets = {
+			{
+				filetype = 'neo-tree',
+				text = 'NeoTree',
+				highlight = 'Directory',
+				separator = true
+			}
+		},
+	}
+})
+
 vim.keymap.set('n', ']', '<Cmd>:BufferLineCycleNext<CR>', { nowait = true } )
 vim.keymap.set('n', '[', '<Cmd>:BufferLineCyclePrev<CR>', { nowait = true } )
 vim.keymap.set('n', '<leader>b', '<Cmd>:BufferLinePick<CR>', { nowait = true } )
